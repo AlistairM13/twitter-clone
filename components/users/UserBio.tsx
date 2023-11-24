fadefdf6623cd505da9aac7731d8353d6d2ca19b
Alistair <alistairm2022@gmail.com>
2023-11-24 13:09:04 +0530

    Edit profile with image upload

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -2,9 +2,10 @@ import { useMemo } from "react";
 import { format } from 'date-fns'
 
 import useCurrentUser from "@/hooks/useCurrentUser";
+import useEditModal from "@/hooks/useEditModal";
 import useUser from "@/hooks/useUser";
-import { BiCalendar } from "react-icons/bi";
 import Button from "../Button";
+import { BiCalendar } from "react-icons/bi";
 
 interface UserBioProps {
     userId: string;
@@ -13,6 +14,8 @@ interface UserBioProps {
 const UserBio: React.FC<UserBioProps> = ({ userId }) => {
     const { data: currentUser } = useCurrentUser()
     const { data: fetchedUser } = useUser(userId)
+
+    const editModal = useEditModal()
 
     const createdAt = useMemo(() => {
         if (!fetchedUser?.createdAt) return null
@@ -25,7 +28,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
         <div className="border-b-[1px] border-neutral-800 pb-4">
             <div className="flex justify-end p-2">
                 {currentUser?.id === userId ? (
-                    <Button secondary label="Edit" onClick={() => { }} />
+                    <Button secondary label="Edit" onClick={()=>editModal.onOpen()} />
                 ) : (
                     <Button
                         onClick={() => { }}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import LoginModal from "@/components/modals/LoginModal"
 import RegisterModal from "@/components/modals/RegisterModal"
 
 import '@/styles/globals.css'
+import EditModal from "@/components/modals/EditModal"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Toaster position="bottom-right"/>
       <RegisterModal />
       <LoginModal />
+      <EditModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
